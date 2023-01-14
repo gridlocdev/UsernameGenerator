5f3fee59e77ad082d19b30e2bac2a1c859199bf9
gridlocdev <gridlocdev@fastmail.com>
2023-01-13 20:06:22 -0700

    Optimized word search algorithm for better performance

--- UsernameGenerator.Console/Program.cs
+++ UsernameGenerator.Console/Program.cs
@@ -2,12 +2,17 @@
 using System.Text.Json;
 using UsernameGenerator.Core;
 
+// The word list is stored in the compiled directory
+var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-var usernameService = new UsernameGeneratorService(
-    JsonSerializer.Deserialize<Word[]>(
-        File.ReadAllText("./Data/words-and-syllables.json")
-    )
+var words = JsonSerializer.Deserialize<Word[]>(
+    File.ReadAllText($"{assemblyPath}/Data/words-and-syllables.json")
 );
+
+if (words is null)
+    throw new Exception("Word list either empty or not found at file location.");
+
+var usernameService = new UsernameGeneratorService(words);
 
 Console.WriteLine(
     "Directions: Press \"Enter\" to advance to the next item, or any other key to stop.\n"
@@ -15,7 +20,11 @@ Console.WriteLine(
 var shouldContinue = true;
 do
 {
-    var username = await usernameService.GetNewCombinationAsync();
+    var username = usernameService.GetNewCombination(
+        usernameLength: 9,
+        firstWordSyllableCount: 1,
+        secondWordSyllableCount: 1
+    );
     Console.WriteLine(username);
     var key = Console.ReadKey(true).Key;
     if (key != ConsoleKey.Enter)
--- UsernameGenerator.Core/UsernameGeneratorService.cs
+++ UsernameGenerator.Core/UsernameGeneratorService.cs
@@ -1,47 +1,59 @@
-using System.Diagnostics;
-using System.Text.Json;
-
 namespace UsernameGenerator.Core;
 
 public class UsernameGeneratorService
 {
-    private byte FirstWordSyllableMinCount { get; set; } = 1;
-    private byte FirstWordSyllableMaxCount { get; set; } = 1;
-    private byte SecondWordSyllableMinCount { get; set; } = 1;
-    private byte SecondWordSyllableMaxCount { get; set;  } = 1;
-    public byte MaxUsernameLength { get; set; } = 9;
-    private string[]? _firstWordList;
-    private string[]? _secondWordList;
-
+    private readonly Word[] _words;
+    public byte ShortestWordLength { get; }
+    public byte LongestWordLength { get; }
+    
     public UsernameGeneratorService(
-       Word[]? words
+        Word[] words
     )
     {
-        _firstWordList = words.Where(x =>
-            x.SyllableCount >= FirstWordSyllableMinCount && x.SyllableCount <= FirstWordSyllableMaxCount).Select(x => x.Name).ToArray();
-        _secondWordList = words.Where(x =>
-            x.SyllableCount >= SecondWordSyllableMinCount && x.SyllableCount <= SecondWordSyllableMaxCount).Select(x => x.Name).ToArray();
+        _words = words;
+        
+        // Mostly all words in the English language are under 255 letters, 
+        ShortestWordLength = (byte)_words.MinBy(w => w.Name.Length)!.Name.Length;
+        LongestWordLength = (byte)_words.MaxBy(w => w.Name.Length)!.Name.Length;
     }
-    
-    public async Task<string> GetNewCombinationAsync()
+
+    public string GetNewCombination(byte usernameLength = 9, byte firstWordSyllableCount = 1,
+        byte secondWordSyllableCount = 1)
     {
-        if (MaxUsernameLength < 2)
-            throw new Exception("The \"MaxUsernameLength\" property is too small, please enter a value 2 or higher.");
+        if (usernameLength < 2)
+            throw new Exception("The \"UsernameLength\" property is too small, please enter a value 2 or higher.");
+        
+        if (usernameLength > LongestWordLength * 2)
+            throw new Exception("The \"UsernameLength\" property is too large for this data set. please enter a smaller word length.");
+        
+        // To increase performance, filter the list of words down to ones that actually fit within the supplied username length
+        var filteredWords = _words.Where(
+            w => w.Name.Length < usernameLength 
+                // Filter out words that have more syllables than both of the two possible syllable counts
+                && w.SyllableCount <= ((firstWordSyllableCount > secondWordSyllableCount) ? firstWordSyllableCount : secondWordSyllableCount)
+        ).ToArray();
+
         string result;
-        byte attempts = 0;
+        Word firstWord;
+        Word secondWord;
+        var attempts = 0;
         do
         {
-            Debug.Assert(_firstWordList != null, nameof(_firstWordList) + " != null");
-            var firstWordIndex = new Random().Next(0, _firstWordList.Length - 1);
-            Debug.Assert(_secondWordList != null, nameof(_secondWordList) + " != null");
-            var secondWordIndex = new Random().Next(0, _secondWordList.Length - 1);
+            firstWord = filteredWords[new Random().Next(0, filteredWords.Length - 1)];
+            secondWord = filteredWords[new Random().Next(0, filteredWords.Length - 1)];
             result =
-                $"{_firstWordList[firstWordIndex]}{_secondWordList[secondWordIndex]}";
-            
+                $"{firstWord.Name}{secondWord.Name}";
+
+            // Prevent an infinite loop by breaking out after a very large (and likely unreasonable to reach) number of retry attempts on the filter criteria
             attempts++;
-            if (attempts >= 50000)
+            if (attempts >= 100000000)
                 break;
-        } while (result.Length > MaxUsernameLength);
+        } while (
+            (result.Length == usernameLength &&
+             firstWord.SyllableCount == firstWordSyllableCount &&
+             secondWord.SyllableCount == secondWordSyllableCount)
+            == false
+        );
 
         return result;
     }
