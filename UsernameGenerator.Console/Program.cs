cb01f3d67b7fedb6452dbfcc0fb70c574b76d1b4
gridlocdev <gridlocdev@fastmail.com>
2024-01-11 20:12:59 -0700

    Migrated console project to use Spectre.Console

--- UsernameGenerator.Console/Program.cs
+++ UsernameGenerator.Console/Program.cs
@@ -1,26 +1,53 @@
-﻿using System.Reflection;
-using System.Text.Json;
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using UsernameGenerator.Core;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
-var usernameService = new UsernameGeneratorService(null);
+var app = new CommandApp<GenerateUsernamesCommand>();
+return app.Run(args);
 
-Console.WriteLine(
-    "Directions: Press \"Enter\" to advance to the next item, or any other key to stop.\n"
-);
-var shouldContinue = true;
-do
+internal sealed class GenerateUsernamesCommand : Command<GenerateUsernamesCommand.Settings>
 {
-    var usernameLength = new Random().Next(2, 12);
-    var firstWordSyllableCount = usernameLength >= 6 ? new Random().Next(1, 2) : 1;
-    var secondWordSyllableCount = usernameLength >= 6 ? new Random().Next(1, 2) : 1;
+    public sealed class Settings : CommandSettings
+    {
+        [Description("The number of total characters in the username")]
+        [CommandOption("--length")]
+        [DefaultValue(null)]
+        public int? UsernameLength { get; init; }
 
-    var username = usernameService.GetNewCombination(
-        usernameLength,
-        firstWordSyllableCount,
-        secondWordSyllableCount
-    );
-    Console.WriteLine(username);
-    var key = Console.ReadKey(true).Key;
-    if (key != ConsoleKey.Enter)
-        shouldContinue = false;
-} while (shouldContinue);
+        [Description("The number of syllables in the first word")]
+        [CommandOption("--word1syllables")]
+        [DefaultValue(null)]
+        public int? FirstWordSyllableCount { get; init; }
+
+        [Description("The number of syllables in the second word")]
+        [CommandOption("--word2syllables")]
+        [DefaultValue(null)]
+        public int? SecondWordSyllableCount { get; init; }
+    }
+
+    public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+    {
+        var usernameService = new UsernameGeneratorService(null);
+
+        Console.WriteLine(
+            "Directions: Press \"Enter\" to advance to the next item, or any other key to stop.\n"
+        );
+        var shouldContinue = true;
+        do
+        {
+            var username = usernameService.GetNewCombination(
+                settings.UsernameLength,
+                settings.FirstWordSyllableCount,
+                settings.SecondWordSyllableCount
+            );
+            Console.WriteLine(username);
+            var key = Console.ReadKey(true).Key;
+            if (key != ConsoleKey.Enter)
+                shouldContinue = false;
+        } while (shouldContinue);
+
+        return 0;
+    }
+}
