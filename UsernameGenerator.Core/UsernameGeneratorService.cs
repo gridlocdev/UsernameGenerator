806a7fdb32eff6ea6d652316f995d7ebc2c8145a
gridlocdev <gridlocdev@fastmail.com>
2024-01-11 20:13:17 -0700

    Added optional arguments support for filter criteria

--- UsernameGenerator.Core/UsernameGeneratorService.cs
+++ UsernameGenerator.Core/UsernameGeneratorService.cs
@@ -9,7 +9,7 @@ public class UsernameGeneratorService
     private Word[] _words;
     private int ShortestWordLength { get; }
     private int LongestWordLength { get; }
-    const int searchLockoutTimeSeconds = 3;
+    private const int searchLockoutTimeSeconds = 3;
 
     public UsernameGeneratorService(
         Word[]? words
@@ -35,14 +35,39 @@ public class UsernameGeneratorService
         return words;
     }
 
-    public string GetNewCombination(int usernameLength = 9, int firstWordSyllableCount = 1,
-        int secondWordSyllableCount = 1)
+    /// <summary>
+    /// A method that randomly generates a username from the specified parameters. Parameters supplied with `null` are randomized.
+    /// </summary>
+    /// <param name="usernameLength">The number of total characters in the username</param>
+    /// <param name="firstWordSyllableCount">The number of syllables in the first word</param>
+    /// <param name="secondWordSyllableCount">The number of syllables in the second word</param>
+    /// <returns>A username in the format "WordWord"</returns>
+    /// <exception cref="UsernameNotPossibleException"></exception>
+    /// <exception cref="TimeoutException"></exception>
+    public string GetNewCombination(
+        int? usernameLength,
+        int? firstWordSyllableCount,
+        int? secondWordSyllableCount
+    )
     {
+        firstWordSyllableCount ??= new Random().Next(1, 2);
+        secondWordSyllableCount ??= new Random().Next(1, 2);
+        // There are no words (that I know of) with 3 letters or less that also contain more than 2 syllables,
+        // this avoids an infinite recursion based on the existing data
+        if ((firstWordSyllableCount > 1 || secondWordSyllableCount > 1) && usernameLength is null)
+            usernameLength = new Random().Next(4, 12);
+        else
+            usernameLength ??= new Random().Next(2, 12);
+
+        if (usernameLength <= 3 && (firstWordSyllableCount > 1 || secondWordSyllableCount > 1))
+            throw new UsernameNotPossibleException(
+                "Too many syllables for username length, please either increase the username length or decrease the syllable count");
+
         if (usernameLength < ShortestWordLength * 2)
-            throw new Exception("The \"UsernameLength\" property is too small, please enter a value 2 or higher.");
+            throw new UsernameNotPossibleException("The \"UsernameLength\" property is too small, please enter a value 2 or higher.");
 
         if (usernameLength > LongestWordLength * 2)
-            throw new Exception(
+            throw new UsernameNotPossibleException(
                 "The \"UsernameLength\" property is too large for this data set. please enter a smaller word length.");
 
         // To increase performance, filter the list of words down to ones that actually fit within the supplied username length
@@ -79,5 +104,26 @@ public class UsernameGeneratorService
 
         stopwatch.Stop();
         return result;
+    }
+
+
+    /// <summary>
+    /// This exception occurs when criteria has been met that prevents the username from being generated due to a lack of possible results
+    /// </summary>
+    private class UsernameNotPossibleException : Exception
+    {
+        public UsernameNotPossibleException()
+        {
+        }
+
+        public UsernameNotPossibleException(string message)
+            : base(message)
+        {
+        }
+
+        public UsernameNotPossibleException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
     }
 }
