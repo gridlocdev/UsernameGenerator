a835ab1c4d2b2f9f72d320a3855b9962c72b42bc
gridlocdev <gridlocdev@fastmail.com>
2024-01-12 21:24:39 -0700

    Add optional arguments support for desktop app

--- UsernameGenerator.Desktop/ViewModels/MainWindowViewModel.cs
+++ UsernameGenerator.Desktop/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,9 @@ public class MainWindowViewModel : ViewModelBase
 {
     private readonly UsernameGeneratorService _service = new UsernameGeneratorService(null);
     private string _username = "";
+    private bool _usernameLengthInputEnabled = true;
+    private bool _firstWordSyllableCountInputEnabled = true;
+    private bool _secondWordSyllableCountInputEnabled = true;
 
     // The Username property must be manually re-raised as it does not represent data from a bound UI control
     public string Username
@@ -19,8 +22,30 @@ public class MainWindowViewModel : ViewModelBase
     public int FirstWordSyllableCount { get; set; } = 1;
     public int SecondWordSyllableCount { get; set; } = 1;
 
+    public bool UsernameLengthInputEnabled
+    {
+        get => _usernameLengthInputEnabled;
+        set => this.RaiseAndSetIfChanged(ref _usernameLengthInputEnabled, value);
+    }
+
+    public bool FirstWordSyllableCountInputEnabled
+    {
+        get => _firstWordSyllableCountInputEnabled;
+        set => this.RaiseAndSetIfChanged(ref _firstWordSyllableCountInputEnabled, value);
+    }
+
+    public bool SecondWordSyllableCountInputEnabled
+    {
+        get => _secondWordSyllableCountInputEnabled;
+        set => this.RaiseAndSetIfChanged(ref _secondWordSyllableCountInputEnabled, value);
+    }
+
     public void Button_OnClick()
     {
-        Username = _service.GetNewCombination(UsernameLength, FirstWordSyllableCount, SecondWordSyllableCount);
+        Username = _service.GetNewCombination(
+            UsernameLengthInputEnabled is true ? UsernameLength : null,
+            FirstWordSyllableCountInputEnabled is true ? FirstWordSyllableCount : null,
+            SecondWordSyllableCountInputEnabled is true ? SecondWordSyllableCount : null
+        );
     }
 }
